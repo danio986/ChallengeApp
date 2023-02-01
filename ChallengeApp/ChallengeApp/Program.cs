99d80191137a99206d7dc5cfe24e3968fe993d03
Daniel <Daniel@DESKTOP-VQ44TAG>
2023-02-01 20:20:51 +0100

    Zadanie domowe dzień 4. Dla płci przyjąłem zmienną bool ze względu na możliwość przyjęcia tylko dwóch wartości, ale zawarłem również drugie rozwiązanie ze zwykłą zmienną.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Pierwsza wersja programu, przy założeniu że zmienna "płeć" może przyjąć tylko dwie wartości.
+
+var name = "Ewa";
+bool sexFemale = true;
+int age = 33;
+
+//var sex = "female";
+
+
+
+if (!sexFemale)
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
+
+if (sexFemale)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta w wieku conajmniej 30 lat");
+    }
+}
+
+// Druga wersja programu w przypadku zwykłej zmiennej "var sex =famel/male".
+
+//if (sex == "female")
+{
+    //  if (name == "Ewa" && age == 33)
+    {
+        //    Console.WriteLine("Ewa, lat 33");
+    }
+    //else if (age < 30)
+    {
+        //  Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    //else if (age >= 30)
+    {
+        //  Console.WriteLine("Kobieta w wieku conajmniej 30 lat");
+    }
+}
+
+//if (sex == "male")
+{
+    //  if (age < 18)
+    {
+        //    Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    //else
+    {
+        //  Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
