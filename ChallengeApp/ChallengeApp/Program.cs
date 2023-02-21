13ddcb6a9d51ff63dbde0a8a850ea2ac02abf1a9
Daniel <Daniel@DESKTOP-VQ44TAG>
2023-02-21 18:52:46 +0100

    baza

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -10,29 +10,9 @@ employee.AddGrade(10.5);
 employee.AddGrade(10);
 employee.AddGrade(8);
 
-var statistics1 = employee.GetStatisticsWithForEach();
+var statistics = employee.GetStatisticsWithForEach();
 Console.WriteLine("Statystyki dla pętli foreach:");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine();
-
-var statistics2 = employee.GetStatisticsWithFor();
-Console.WriteLine("Statystyki dla pętli for:");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-Console.WriteLine();
-
-var statistics3 = employee.GetStatisticsWithWhile();
-Console.WriteLine("Statystyki dla pętli While:");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-Console.WriteLine();
-
-var statistics4 = employee.GetStatisticsWithDoWhile();
-Console.WriteLine("Statystyki dla pętli DoWhile:");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
